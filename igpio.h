75846600ca6386f05f2c2c574ee1ae0618c9652d
Martyn Davis <mcd@marengo.cc>
2019-07-16 11:28:21 +0100

    Added catch.hpp for unit testing

--- igpio.h
+++ igpio.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// Abstract base "interface" class
+// to allow mocking out the hardware
+// interface to allow the StepperMotor
+// class to be used for tests
+
 class IGpio
 {
     // TODO
