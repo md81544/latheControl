24d2d1474ac3a68519668c3a9272ec3ff0df0e59
Martyn Davis <mcd@marengo.cc>
2019-07-17 08:45:28 +0100

    Update igpio ABC

--- igpio.h
+++ igpio.h
@@ -5,7 +5,22 @@
 // interface to allow the StepperMotor
 // class to be used for tests
 
+namespace mgo
+{
+
+enum class PinState
+{
+    High,
+    Low
+};
+
+// concrete classes can initialise / terminate
+// the GPIO library in their ctors / dtors
+
 class IGpio
 {
-    // TODO
+    void setStepPin( PinState ) = 0;
+    void setReversePin( PinState ) = 0;
 };
+
+} // end namespace
